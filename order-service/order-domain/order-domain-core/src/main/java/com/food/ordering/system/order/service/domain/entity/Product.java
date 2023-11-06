1cf4af814c977b094bc9a67bbe24ef7c376064fd
Yusuf Burak Gunes <yburakgunes93@gmail.com>
2023-11-06 14:48:08 +0300

    Initial Commit

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -0,0 +1,33 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.ProductId;
+
+public class Product extends BaseEntity<ProductId> {
+    private String name;
+    private Money price;
+
+    public Product(ProductId id, String name, Money price) {
+        super.setId(id);
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(ProductId id) {
+        super.setId(id);
+    }
+
+    public void updateWithConfirmedNameAndPrice(String name, Money price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+}
